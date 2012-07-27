fb419161c2fa00fbb1bbb22590c57db75dd70631
jim@zope.com <none@none>
2012-07-27 21:20:30 +0000

    Removed the timeout logic:
    - It's problematic to time tings out at this level.  It's probably
      better to handle this through monitoring.
    
    (Along these lines, we should probably use a longer timeout for the
      zdaemon start-test-program. I'd rather call a human for help than
      just march on if a process doesn't start.)
    
    - This feature was due to a missunderstanding.
    
    - Testing it is slow. :)
    
    --HG--
    extra : convert_revision : svn%3A32cb22c4-c7e1-0310-b164-a889846e9adb/zc.zkdeployment/trunk%4071519

--- src/zc/zkdeployment/__init__.py
+++ src/zc/zkdeployment/__init__.py
@@ -4,42 +4,25 @@ import subprocess
 import tempfile
 import zc.thread
 
-TIMEOUT_INTERVAL = 900 # seconds
-
 logger = logging.getLogger(__name__)
 
-
-def run_command(cmd_list, timeout=None, verbose=False):
-    timeout = timeout or TIMEOUT_INTERVAL
+def run_command(cmd_list, verbose=False):
     tfile = tempfile.NamedTemporaryFile('w', delete=False,
                                         prefix='zkdeployment-run_command')
-    process = []
-    @zc.thread.Thread
-    def worker():
-        process.append(subprocess.Popen(
-            cmd_list, stdout=tfile, stderr=subprocess.STDOUT))
-        process[0].communicate()
-    worker.join(timeout)
-    if worker.is_alive():
-        process[0].terminate()
-        raise RuntimeError('Command failed: ' + ' '.join(cmd_list) +
-                ', took too long')
+    process = subprocess.Popen(
+            cmd_list, stdout=tfile, stderr=subprocess.STDOUT)
+    process.communicate()
     tfile.close()
     with open(tfile.name) as f:
         output = f.read()
     os.remove(tfile.name)
-
-    if worker.exception:
-        raise worker.exception
-
-    if process:
-        if process[0].returncode != 0:
-            logger.error("Command failed: %r\n  %s" %
-                            (cmd_list,
-                             output.replace('\n', '\n  ').strip()))
-            raise RuntimeError('Command failed: ' + ' '.join(cmd_list))
-        elif verbose:
-            logger.info("Command succeeded: %r\n  %s" %
+    if process.returncode != 0:
+        logger.error("Command failed: %r\n  %s" %
+        (cmd_list,
+         output.replace('\n', '\n  ').strip()))
+        raise RuntimeError('Command failed: ' + ' '.join(cmd_list))
+    elif verbose:
+        logger.info("Command succeeded: %r\n  %s" %
                     (cmd_list,
                      output.replace('\n', '\n  ').strip()))
 
