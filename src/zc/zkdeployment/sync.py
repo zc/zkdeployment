41d5075c07cbfc4bf330002acfef676d88db8e07
Jim Fulton <jim@zope.com>
2013-11-18 18:20:39 -0500

    Refactored to isolate svn logic.

--- src/zc/zkdeployment/sync.py
+++ src/zc/zkdeployment/sync.py
@@ -6,7 +6,6 @@ import zc.zkdeployment
 import zktools.locking
 import zookeeper
 
-SVN_CMD = 'svn'
 ZK_LOCATION = 'zookeeper:2181'
 
 logger = logging.getLogger(__name__)
@@ -15,15 +14,33 @@ logger = logging.getLogger(__name__)
 zookeeper.set_log_stream = lambda f: None
 
 
-def svn_cmd(cmd, url):
-    return zc.zkdeployment.run_command([SVN_CMD, cmd, url], return_output=True)
 
+def svn_cmd(cmd, url): # This exists to be mocked
+    return zc.zkdeployment.run_command(['svn', cmd, url], return_output=True)
 
-def get_svn_version(body):
-    for line in body.splitlines():
-        if line.startswith('Last Changed Rev:'):
-            return int(line.split()[-1])
+class SVN:
 
+    def __init__(self, url):
+        self.url = url
+        self.version = self.get_version()
+
+    def __call__(self, cmd, url=None):
+        return svn_cmd(cmd, url or self.url)
+
+    def get_version(self):
+        for line in self('info').splitlines():
+            if line.startswith('Last Changed Rev:'):
+                return int(line.split()[-1])
+
+    def __iter__(self):
+        allfiles = [fi for fi in self('ls').strip().split('\n')]
+        zkfiles = [fi for fi in allfiles if fi.endswith('.zk')]
+        zkxfiles = [fi for fi in allfiles if fi.endswith('.zkx')]
+
+        # Import changes
+        for fi in zkfiles + zkxfiles:
+            contents = self('cat', '%s/%s' % (self.url,  fi))
+            yield (fi, contents)
 
 def get_zk_version(zk):
     try:
@@ -40,32 +57,28 @@ def sync_with_canonical(url, dry_run=False, force=False):
         if not force:
             return
 
-    info_body = svn_cmd('info', url)
-    svn_version = get_svn_version(info_body)
-    logger.info("VCS Version: " + str(svn_version))
+    vcs = SVN(url)
+    logger.info("VCS Version: " + str(vcs.version))
     logger.info("ZK Version: " + str(zk_version))
-    if zk_version != svn_version:
-        for child in zk.get_children('/hosts'):
-            host_version = zk.properties('/hosts/' + child)['version']
-            if host_version != zk_version and not force:
-                logger.error(
-                    "Version mismatch detected, can't resync since " +
-                    "host %s has not converged (%s -> %s)" % (
-                        child, host_version, zk_version))
-                return
+    if zk_version != vcs.version:
+        if not force:
+            for child in zk.get_children('/hosts'):
+                host_version = zk.properties('/hosts/' + child)['version']
+                if host_version != zk_version:
+                    logger.error(
+                        "Version mismatch detected, can't resync since " +
+                        "host %s has not converged (%s -> %s)" % (
+                            child, host_version, zk_version))
+                    return
+
         cluster_lock = zktools.locking.ZkLock(zk, ',hosts')
         try:
             if cluster_lock.acquire(0):
                 logger.info("Version mismatch detected, resyncing")
 
-                allfiles = [fi for fi in svn_cmd('ls', url).strip().split('\n')]
-                zkfiles = [fi for fi in allfiles if fi.endswith('.zk')]
-                zkxfiles = [fi for fi in allfiles if fi.endswith('.zkx')]
-
                 # Import changes
-                for fi in zkfiles + zkxfiles:
+                for fi, contents in vcs:
                     output = ' '.join(('Importing', fi))
-                    contents = svn_cmd('cat', '%s/%s' % (url,  fi))
                     if dry_run:
                         output += ' (dry run, no action taken)'
                     logger.info(output)
@@ -73,7 +86,7 @@ def sync_with_canonical(url, dry_run=False, force=False):
                         zk.import_tree(contents, trim=fi.endswith('.zk'))
                 # bump version number
                 if not dry_run:
-                    zk.properties('/hosts').update(version=svn_version)
+                    zk.properties('/hosts').update(version=vcs.version)
             else:
                 logger.error("Refused to update zookeeper tree, "
                     "couldn't obtain cluster lock")
